9ae3eee41993a36e738dfd6eede1330ae81af922
Syed Ashar <saghirashar@gmail.com>
2021-10-30 16:23:27 +0500

    layout

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 import MainPage from './screens/MainPage';
 import React, { Component } from 'react'
+import TestPage from './screens/TestPage';
 
 export default class App extends Component {
   render() {
@@ -11,6 +12,7 @@ export default class App extends Component {
                         
             <main>
                 <Route path="/" component={MainPage} exact></Route>
+                <Route path="/test" component={TestPage} exact></Route>
             </main>
         </div>
     </BrowserRouter>
---
+++ src/screens/TestPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from '../components/dashboard/Layout'
+
+
+export default function TestPage() {
+    return (
+        <Layout>
+        <h1>Hi this is the test page.</h1>
+        </Layout> 
+    )
+}
